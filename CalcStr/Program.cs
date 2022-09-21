f2e73a1ebd938865d6e90f5c1825a134387b7ba4
AndrewPopoff <andrew.popoff@gmail.com>
2022-09-21 21:49:22 +1000

    добавлен проект работы со строками

---
+++ CalcStr/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace CalcStr
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str1 = "Привет, ";
+            string str2 = "мир!";
+            string str = str1 + str2;
+            Console.WriteLine(str);
+
+            int len = str1.Length;
+            Console.WriteLine(len);
+
+            char a = 'a';
+            char b = 'b';
+            int aInt = (int)a;
+            int bInt = (int)b;
+            char c = (char)((int)b + 1);
+
+            for(int i = aInt;i<= (int)'z'; i ++)
+            {
+                Console.Write((char)i);
+            }
+            Console.WriteLine();
+
+            char cFirst = str[0];
+            char cLast = str[str.Length - 1];
+
+            for(int i =0; i< str.Length; i++)
+            {
+                Console.Write(str[i] + " ");
+            }
+            Console.WriteLine();
+
+            str = str.Remove(1, 1);
+            Console.WriteLine(str);
+
+            str = str.Remove(str.Length - 1, 1);
+            Console.WriteLine(str);
+        }
+    }
+}
